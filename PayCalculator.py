dae6c84a7b8be7eac0b07e4c9e47205eb905d228
laffeytaffey300 <133976283+laffeytaffey300@users.noreply.github.com>
2023-05-20 14:04:36 -0400

    Update PayCalculator.py
    Added comments.
    Separated variable assignment and conversion to float to allow for error handling.
    Error handling when input is not numeric.
    
    try/except
    quit()

--- PayCalculator.py
+++ PayCalculator.py
@@ -1,5 +1,14 @@
+#asks user to enter hours and rate
+#prints error if input for hours or rate is not numeric
 hours = float(input('Enter Hours:'))
-rate = float(input('Enter Rate:'))
+try:
+    hours=float(hours)
+    rate = input('Enter Rate:')
+    rate = float(rate)
+except:
+    print('Error, please enter numeric input')
+    quit()
+#calculates pay with 1.5*rate over 40 hours
 if hours > 40:
     print('Pay:', 40*rate+((hours-40)*(rate*1.5)))
 else: print('Pay:', hours*rate)
