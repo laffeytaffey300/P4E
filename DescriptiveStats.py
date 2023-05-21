3b4c794ea029fcfcaebcd8578950ac7ae271b893
laffeytaffey300 <133976283+laffeytaffey300@users.noreply.github.com>
2023-05-20 20:49:51 -0400

    Update DescriptiveStats.py
    -Added max and min variables
    -Moved break condition outside of exception handling
    -Moved updates to sum and count variables outside of try block
    -Added continue at end of except block to account for additional code in loop after except block
    -Added logic to update min and max on first and subsequent iterations through loop
    -Added min and max to print statement
    -Consolidated print statements for success path into one using \n for line breaks

--- DescriptiveStats.py
+++ DescriptiveStats.py
@@ -1,22 +1,34 @@
 #defines starting values
 sum = 0
 count = 0
+max = None
+min = None
 
 #loop
 while True:
     input_val= input('Enter a number: ')
+    if input_val == 'done': 
+        break #allows user to exit loop by entering 'done'
     try:
         float_num = float(input_val) #checks if input value is number
-        sum = sum + float_num
-        count = count + 1
     except:
-        if input_val == 'done': 
-            break #allows user to exit loop by entering 'done'
-        print('bad data') #exception handingling when input is not a number
+        print('bad data') #exception handling when input is not a number
+        continue #skips updating stats for bad input
+    sum = sum + float_num
+    if count == 0: #sets max and min to first input value on first iteration
+            max = float_num
+            min = float_num
+    else:
+        if float_num > max: max = float_num
+        if float_num < min: min = float_num
+    count = count + 1
 
 #print output
 if count > 0: #checks at least one valid entry has been made
-    print('Total:', sum)
-    print('Count:', count)
-    print('Average:', sum/count)
-else: print("Oops! You didn't enter any valid data before entering 'done'" ) #error message if no valid entries
+    print(
+        '\nTotal:', sum,
+        '\nCount:', count,
+        '\nAverage:', sum/count,
+        '\nMax:', max,
+        '\nMin:', min)
+else: print("\nOops! You didn't enter any valid data before entering 'done', there's nothing to calculate!" ) #error message if no valid entries
